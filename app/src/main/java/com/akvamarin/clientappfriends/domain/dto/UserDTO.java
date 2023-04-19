537ad76a8b8e7a59ee1944d0db256ed5fe6a2186
Akvamarinika <rozespink@mail.ru>
2023-04-19 20:11:33 +0800

    added userDTO

---
+++ app/src/main/java/com/akvamarin/clientappfriends/domain/dto/UserDTO.java
@@ -0,0 +1,54 @@
+package com.akvamarin.clientappfriends.domain.dto;
+
+import com.akvamarin.clientappfriends.domain.enums.Alcohol;
+import com.akvamarin.clientappfriends.domain.enums.Psychotype;
+import com.akvamarin.clientappfriends.domain.enums.Sex;
+import com.akvamarin.clientappfriends.domain.enums.Smoking;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Set;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO implements Serializable {
+
+    private Long id;
+
+    private String email;
+
+    private String username;
+
+    private String phone;
+
+    private String password;
+
+    private LocalDate dateOfBirthday;
+
+    private String nickname;
+
+    private Sex sex;
+
+    private String aboutMe;
+
+    private Smoking smoking;
+
+    private Alcohol alcohol;
+
+    private Psychotype psychotype;
+
+    private String urlAvatar;
+
+    private Long cityID;
+
+    private Set<String> roles;
+
+    private String vkId;
+
+}
