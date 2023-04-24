d68a546504b4ce8912cadd7ade6f69970abed46b
Akvamarinika <rozespink@mail.ru>
2023-04-24 17:40:45 +0800

    added request uploadUsersFromServerDatabase()

--- app/src/main/java/com/akvamarin/clientappfriends/view/chats/AddUsersActivity.java
+++ app/src/main/java/com/akvamarin/clientappfriends/view/chats/AddUsersActivity.java
@@ -1,21 +1,36 @@
 package com.akvamarin.clientappfriends.view.chats;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.akvamarin.clientappfriends.API.RetrofitService;
+import com.akvamarin.clientappfriends.API.connection.UserApi;
 import com.akvamarin.clientappfriends.adapters.UserAdapter;
 import com.akvamarin.clientappfriends.databinding.ActivityAddUsersBinding;
-import com.akvamarin.clientappfriends.domain.dto.User;
+import com.akvamarin.clientappfriends.domain.dto.ViewUserSlimDTO;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AddUsersActivity extends AppCompatActivity {
+    private static final String TAG = "recyclerSlimUsers";
     private ActivityAddUsersBinding binding;
+    private RetrofitService retrofitService;
+    private UserApi userApi;
+    private static List<ViewUserSlimDTO> userList = null;
 
 
     @Override
@@ -25,7 +40,11 @@ public class AddUsersActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         //setContentView(R.layout.activity_add_users);
         setListeners();
-        uploadUsersFromDatabase();
+        init();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            uploadUsersFromServerDatabase();
+        }
 
     }
 
@@ -33,37 +52,43 @@ public class AddUsersActivity extends AppCompatActivity {
        binding.chatsImageBack.setOnClickListener(view -> {
            onBackPressed();
        });
-
-
     }
 
-    private void uploadUsersFromDatabase(){
+    private void init(){
+        retrofitService = RetrofitService.getInstance(getApplicationContext());
+        userApi = retrofitService.getRetrofit().create(UserApi.class);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void uploadUsersFromServerDatabase(){
         binding.chatsProgressBar.setVisibility(View.INVISIBLE);
         //loading(true);
-        List<User> userList = new ArrayList<>();
 
-        User user1 = new User("Виктор", 28, "https://meragor.com/files/styles//ava_800_800_wm/sfztn_boy_avatar_1.jpg");
-        user1.setId(1);
-        User user2 = new User("Анна", 25, "https://meragor.com/files/styles//ava_800_800_wm/_5_5.jpg");
-        User user3 = new User("Александр", 23, "https://demotivation.ru/wp-content/uploads/2020/11/905e85b5e1f2b5e1935c81b3c2478829.jpg");
-        User user4 = new User("Виктория", 20, "https://meragor.com/files/styles//ava_800_800_wm/avatar-211226-001768.png");
-        User user5 = new User("Инна", 31, "https://meragor.com/files/styles//ava_800_800_wm/avatar-210866-000320.png");
-        User user6 = new User("Алекс", 35, "https://meragor.com/files/styles//ava_800_800_wm/sfztn_boy_avatar_18.jpg");
-        User user7 = new User("Марина", 27, "https://meragor.com/files/styles//ava_800_800_wm/2_8.jpg");
-        User user8 = new User("Пётр", 27, "https://it-doc.info/wp-content/uploads/2019/06/avatar-9.jpg");
-        User user9 = new User("Никита", 25, "https://it-doc.info/wp-content/uploads/2019/06/avatar-guitar-man.jpg");
-        User user10 = new User("Николай", 32, "https://meragor.com/files/styles//ava_800_800_wm/sfztn_boy_avatar_64.jpg");
 
-        userList.add(user1);
-        userList.add(user2);
-        userList.add(user3);
-        userList.add(user4);
-        userList.add(user5);
-        userList.add(user6);
-        userList.add(user7);
-        userList.add(user8);
-        userList.add(user9);
-        userList.add(user10);
+        Log.d(TAG, "init slimUserDTO list: preparing ...");
+
+        if (userList == null) {
+            userList = new ArrayList<>();
+
+            userApi.getAllSlimUsers().enqueue(new Callback<>() {
+                @Override
+                public void onResponse(@NonNull Call<List<ViewUserSlimDTO>> call, @NonNull Response<List<ViewUserSlimDTO>> response) {
+
+                    if (response.isSuccessful()) {
+                        userList = response.body();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "getSlimUserDTOs() code:" + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(@NonNull Call<List<ViewUserSlimDTO>> call, @NonNull Throwable t) {
+                    Toast.makeText(getApplicationContext(), "getSlimUserDTOs() --- Fail", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "error: " + t.fillInStackTrace());
+                }
+
+            });
+        }
 
         if (userList.size() > 0){
             UserAdapter userAdapter = new UserAdapter(userList);
@@ -73,7 +98,7 @@ public class AddUsersActivity extends AppCompatActivity {
             binding.fabBtnSelectedUsers.setOnClickListener(new View.OnClickListener() { ///////////////////////////// перенести
                 @Override
                 public void onClick(View view) {
-                    List<User> selectedUsers;
+                    List<ViewUserSlimDTO> selectedUsers;
                     selectedUsers = userAdapter.getSelectedUsers();
 
                     Intent intent = new Intent(binding.getRoot().getContext(), GroupChatActivity.class);
