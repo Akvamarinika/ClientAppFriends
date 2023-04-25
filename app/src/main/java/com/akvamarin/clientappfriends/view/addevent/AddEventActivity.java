13951eb16363a2315524e7d1fb5ef14f88d2149f
Akvamarinika <rozespink@mail.ru>
2023-04-25 22:56:32 +0800

    added requests EventDTO send on Server, uploadSpinnerEventCategoriesFromServer()

--- app/src/main/java/com/akvamarin/clientappfriends/view/addevent/AddEventActivity.java
+++ app/src/main/java/com/akvamarin/clientappfriends/view/addevent/AddEventActivity.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -21,45 +20,51 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import com.akvamarin.clientappfriends.domain.enums.Partner;
-import com.akvamarin.clientappfriends.view.AllEventsActivity;
+import com.akvamarin.clientappfriends.API.ErrorResponse;
+import com.akvamarin.clientappfriends.API.ErrorUtils;
+import com.akvamarin.clientappfriends.API.RetrofitService;
+import com.akvamarin.clientappfriends.API.connection.EventApi;
+import com.akvamarin.clientappfriends.API.connection.EventCategoryApi;
 import com.akvamarin.clientappfriends.R;
-import com.akvamarin.clientappfriends.domain.dto.Event;
-import com.akvamarin.clientappfriends.domain.dto.User;
+import com.akvamarin.clientappfriends.domain.dto.AuthToken;
+import com.akvamarin.clientappfriends.domain.dto.EventCategoryDTO;
+import com.akvamarin.clientappfriends.domain.dto.EventDTO;
 import com.akvamarin.clientappfriends.domain.enums.DayPeriodOfTime;
 import com.akvamarin.clientappfriends.domain.enums.Partner;
-import com.akvamarin.clientappfriends.view.ui.home.HomeAllEventsFragment;
 import com.akvamarin.clientappfriends.utils.Constants;
 import com.akvamarin.clientappfriends.utils.PreferenceManager;
+import com.akvamarin.clientappfriends.view.AllEventsActivity;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AddEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
-    private static final String TAG = "ADD";
+    private static final String TAG = "AddEvent";
     private static final int MIN_TEXT_LENGTH = 1;
     private static final int MAX_COUNT_LINE = 10;
     private static final String EMPTY_STRING = "";
 
     private Toolbar toolbar;
-    private MenuItem searchMenuItem;
-    private MenuItem filterMenuItem;
-    private MenuItem arrowBackMenuItem;
+    //private MenuItem searchMenuItem;
+    //private MenuItem filterMenuItem;
+    //private MenuItem arrowBackMenuItem;
 
     private Spinner spinnerEventCategories;
 
@@ -79,6 +84,11 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
 
     private PreferenceManager preferenceManager;
 
+    private List<EventCategoryDTO> categoriesList = new ArrayList<>();
+    private RetrofitService retrofitService;
+    private EventCategoryApi categoryApi;
+    private EventApi eventApi;
+
     @SuppressLint("ClickableViewAccessibility")
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -95,7 +105,7 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        initSpinnerEventCategories();
+        uploadSpinnerEventCategoriesFromServer();
         //getSelectedItemFromSpinnerEventCategories();
 
         buttonShowDialogDatePicker.setOnClickListener(view -> showSetDateDialog()); // new View.OnClickListener(){ /*method onClick*/ }
@@ -105,46 +115,11 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
 
         buttonSave.setOnClickListener(view -> {
             if (checkTextFieldsInFormOnCount()){
-                String category = spinnerEventCategories.getSelectedItem().toString();
-                String eventName = editTextEventName.getText().toString().trim();
-                String eventDescription = editTextEventDescription.getText().toString().trim();
-                LocalDate date = Instant.ofEpochMilli(calendar.getTimeInMillis()).atZone(ZoneId.systemDefault()).toLocalDate();
 
-                int radioIDPartner = radioGroupPartner.getCheckedRadioButtonId();
-                RadioButton radioBtnPartner = radioGroupPartner.findViewById(radioIDPartner);
-                String partnerText = radioBtnPartner.getText().toString();
-                Log.d(TAG, "radio partner: " + partnerText);
-
-                int radioIDTwentyFourHours  = radioGroupTwentyFourHours.getCheckedRadioButtonId();
-                RadioButton radioBtnTwentyFourHours = radioGroupTwentyFourHours.findViewById(radioIDTwentyFourHours);
-                String periodOfTimeText = radioBtnTwentyFourHours.getText().toString();
-                Log.d(TAG, "radio  periodOfTime" + periodOfTimeText);
-
-                Event event = new Event();
-                event.setCategory(category);
-                event.setEventName(eventName);
-                event.setEventDescription(eventDescription);
-                event.setDate(date);
-                event.setPartner(Partner.getEnumValue(partnerText));
-                event.setPeriodOfTime(DayPeriodOfTime.getEnumValue(periodOfTimeText));
-                event.setDateTimeCreated(LocalDateTime.now());
-
-                /*TODO: добавить данные авторизированного пользов-ля */
-                //event.setUser(new User("Test app", 27, "https://android-obzor.com/wp-content/uploads/2022/03/dc715986ea8bc0a25ea8655e6c2c1386.jpg"));
-
-                String name = preferenceManager.getString(Constants.KEY_NAME);
-                String age = preferenceManager.getString(Constants.KEY_AGE);
-                String email = preferenceManager.getString(Constants.KEY_EMAIL);
-                User userAutorizes = new User(name, Integer.parseInt(age), ""); //https://android-obzor.com/wp-content/uploads/2022/03/dc715986ea8bc0a25ea8655e6c2c1386.jpg
-                userAutorizes.setEmail(email);
-                event.setUser(userAutorizes);
-
-                Log.d(TAG, "onClick: " + event);
-
-                /*TODO: event отправить на сервер */
+                EventDTO event = setValuesForEventDTO();
+                sendNewEventOnServer(event); //event отправить на сервер
 
              //   HomeAllEventsFragment.getEventList().add(event);
-
                 Intent intent = new Intent(this, AllEventsActivity.class);
                 //intent.putExtra("Event_obj", event);
                 startActivity(intent);
@@ -165,8 +140,6 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
             return false;
         });
 
-
-
     }
 
     private void initWidgets(){
@@ -181,37 +154,105 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
         buttonSave = findViewById(R.id.buttonSaveNewEvent);
 
         preferenceManager = new PreferenceManager(getApplicationContext());
-
+        retrofitService = RetrofitService.getInstance(getApplicationContext());
+        categoryApi = retrofitService.getRetrofit().create(EventCategoryApi.class);
+        eventApi = retrofitService.getRetrofit().create(EventApi.class);
     }
 
-    /*** Spinner category methods: ***/
-    private void initSpinnerEventCategories(){
+
+    /*** Spinner category methods:
+     * Получение значений от Сервера
+     * ***/
+    private void uploadSpinnerEventCategoriesFromServer(){
         spinnerEventCategories = findViewById(R.id.eventCategoriesSpinner);
 
-        String[] stringArrayCategories = getResources().getStringArray(R.array.event_categories_lst);
-        List<String> listCategories = Arrays.asList(stringArrayCategories);
-        Collections.sort(listCategories); //sorted
+        if (categoriesList.isEmpty()) {
+            categoryApi.getAllCategories().enqueue(new Callback<>() {
+                @Override
+                public void onResponse(@NonNull Call<List<EventCategoryDTO>> call, @NonNull Response<List<EventCategoryDTO>> response) {
+                    if (response.isSuccessful()) {
+                        categoriesList = response.body();
+                        ArrayAdapter<?> adapter = new ArrayAdapter<>(AddEventActivity.this, android.R.layout.simple_spinner_item, categoriesList);
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        spinnerEventCategories.setAdapter(adapter);
+                        Log.d(TAG, "categoriesList size: " + categoriesList.size());
+                    } else {
+                        Toast.makeText(getApplicationContext(), "getAllCategoriesDTOs() code:" + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                }
 
-        ArrayAdapter<?> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listCategories);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerEventCategories.setAdapter(adapter);
+                @Override
+                public void onFailure(@NonNull Call<List<EventCategoryDTO>> call, @NonNull Throwable t) {
+                    Toast.makeText(AddEventActivity.this, "getAllCategoriesDTOs() filed!!!", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "error: " + t.fillInStackTrace());
+                }
+            });
+        }
     }
 
-//    private void getSelectedItemFromSpinnerEventCategories(){
-//        spinnerEventCategories.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> adapterView, View view, int selectedItemPosition, long selectedId) {
-//                String itemSelected = adapterView.getItemAtPosition(selectedItemPosition).toString();
-//                Log.d(TAG, "onItemSelected: " + itemSelected );
-//                Toast toast = Toast.makeText(getApplicationContext(),
-//                        "Ваш выбор: " + itemSelected , Toast.LENGTH_SHORT);
-//                toast.show();
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> adapterView) { }
-//        });
-//    }
+    /*** Заполнение объекта EventDTO
+     * из формы:
+     * ***/
+    private EventDTO setValuesForEventDTO(){
+        EventCategoryDTO selectedCategory = (EventCategoryDTO) spinnerEventCategories.getSelectedItem();
+        Long categoryId = selectedCategory.getId();
+
+        String eventName = editTextEventName.getText().toString().trim();
+        String eventDescription = editTextEventDescription.getText().toString().trim();
+
+        //LocalDate date = Instant.ofEpochMilli(calendar.getTimeInMillis()).atZone(ZoneId.systemDefault()).toLocalDate();
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = dateFormat.format(calendar.getTime());
+
+        int radioIDPartner = radioGroupPartner.getCheckedRadioButtonId();
+        RadioButton radioBtnPartner = radioGroupPartner.findViewById(radioIDPartner);
+        String partnerText = radioBtnPartner.getText().toString();
+        Log.d(TAG, "radio partner: " + partnerText);
+
+        int radioIDTwentyFourHours  = radioGroupTwentyFourHours.getCheckedRadioButtonId();
+        RadioButton radioBtnTwentyFourHours = radioGroupTwentyFourHours.findViewById(radioIDTwentyFourHours);
+        String periodOfTimeText = radioBtnTwentyFourHours.getText().toString();
+        Log.d(TAG, "radio  periodOfTime" + periodOfTimeText);
+
+        EventDTO event = new EventDTO();
+        event.setEventCategoryId(categoryId); //id
+        event.setName(eventName);
+        event.setDescription(eventDescription);
+        event.setDate(dateString); //format "yyyy-MM-dd"
+        event.setPartner(Partner.getEnumValue(partnerText));
+        event.setPeriodOfTime(DayPeriodOfTime.getEnumValue(periodOfTimeText));
+
+        event.setOwnerId(Long.valueOf("-1")); // назначит Владельца сервер по токену, чтобы значение было не null
+        Log.d(TAG, "onClick: " + event);
+
+        return event;
+    }
+
+    /*** EventDTO send on Server:
+     * ***/
+    private void sendNewEventOnServer(EventDTO event){
+        String authToken = preferenceManager.getString(Constants.KEY_APP_TOKEN);
+
+        eventApi.createEvent(event, new AuthToken(authToken)).enqueue(new Callback<>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+                Log.d(TAG, String.format("Response: %s %s%n", response.code(), response));
+                Log.d(TAG, String.format("My token: %s", authToken));
+
+                if(response.isSuccessful()){
+                    Log.d(TAG, response.code() + " event added");
+                } else {
+                    ErrorResponse error = ErrorUtils.parseError(response, retrofitService);
+                    Log.d(TAG, error.getStatusCode() + " " + error.getMessage());
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+                Log.d(TAG, "error: " + t.fillInStackTrace());
+            }
+        });
+    }
 
     /*** Methods for Date field: ***/
     private void setNewDate(){
@@ -303,24 +344,8 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
     @Override
     public boolean onSupportNavigateUp() {  // Toolbar: backArrow
         onBackPressed();    //нажатие на кнопку назад внизу экрана
+        finish(); // close the current activity
         return true;
     }
 
 }
-
-
-//        editTextEventName.addTextChangedListener(new TextChangedListener<>(editTextEventName) {
-//@Override
-//public void onTextChanged(EditText editText, Editable s) {
-//        Toast toast = Toast.makeText(getApplicationContext(),
-//        "EditText: " + editText + "Editable: " + s, Toast.LENGTH_SHORT);
-//        // toast.show();
-//        }
-//        });
-//
-//        editTextEventDescription.addTextChangedListener(new TextChangedListener<>(editTextEventDescription) {
-//@Override
-//public void onTextChanged(EditText editText, Editable s) {
-//
-//        }
-//        });
