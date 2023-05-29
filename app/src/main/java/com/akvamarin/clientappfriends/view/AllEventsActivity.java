fb80d11c03ac34da0fbafe77278cc64af7c1ca3b
Akvamarinika <rozespink@mail.ru>
2023-05-30 01:22:04 +0800

    refactoring

--- app/src/main/java/com/akvamarin/clientappfriends/view/AllEventsActivity.java
+++ app/src/main/java/com/akvamarin/clientappfriends/view/AllEventsActivity.java
@@ -3,13 +3,10 @@ package com.akvamarin.clientappfriends.view;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.akvamarin.clientappfriends.BaseActivity;
 import com.akvamarin.clientappfriends.R;
